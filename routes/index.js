9500eea0872565be2035658d5bd04ba76d04ea7e
Shakalee14 <shakalee14@gmail.com>
2016-09-27 11:13:36 -0700

    added emoji library with beautiful black emojis

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,9 @@ var emoji = require('node-emoji');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  console.log(emoji.get('coffee'));
-  res.render('index', { title: 'Emoji Cipher' });
+  console.log(emoji.get('blackprincess'));
 
+  res.render('index', { title: 'Emoji Cipher' });
 });
 
 module.exports = router;
