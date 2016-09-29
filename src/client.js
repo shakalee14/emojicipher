cd7ddac78a63b318836167136d07332cda578d7d
Shakalee14 <shakalee14@gmail.com>
2016-09-28 23:24:58 -0700

    wip onclick

---
+++ src/client.js
@@ -0,0 +1,8 @@
+var input = document.querySelector('#my-text-box');
+
+var messages = document.querySelector('#messages');
+
+input.addEventListener('input', function()
+{
+    messages.textContent += 'input changed to: ' + input.value + '\n';
+});
